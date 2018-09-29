c964c2faaed5b7bb3979e9ed05d4bb3cce39c83f
Shailyagarwal <38818884+Shailyagarwal@users.noreply.github.com>
2018-09-29 15:21:11 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+          // Hide the div
+          $(".card-1").hide();
+          // Show the div after 5s
+          $(".card-1").delay(1000).fadeIn(500);
+
+          $(".card-2").hide();
+          // Show the div after 5s
+          $(".card-2").delay(2000).fadeIn(500);
+
+          $(".card-3").hide();
+          // Show the div after 5s
+          $(".card-3").delay(3000).fadeIn(500);
+
+          $(".card-4").hide();
+          // Show the div after 5s
+          $(".card-4").delay(4000).fadeIn(500);
+
+          $(".card-5").hide();
+          // Show the div after 5s
+          $(".card-5").delay(5000).fadeIn(500);  
+      });  
